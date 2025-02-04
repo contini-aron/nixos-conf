b78b88fe5f26249f65690d0d9626d5f7e445f2ac
contini-aron <contini.aron99@protonmail.com>
2025-02-04 09:56:10 +0100

    stable: 04/02/2025

--- pkgs/nvf/package.nix
+++ pkgs/nvf/package.nix
@@ -22,6 +22,7 @@
       python.enable = true;
       rust.enable = true;
       go.enable = true;
+      terraform.enable = true;
       # enables treesitter by default for enabled languages
       enableTreesitter = true;
       # enables LSP by default for enabled languages
